c2a15846ef51c070d0e26dddfc18d4db78071750
Kamil Seweryn <kamil.seweryn@vimn.com>
2017-02-03 20:39:34 +0100

    error reproduced

--- app/src/main/java/srw/pl/issue220795/MainActivity.java
+++ app/src/main/java/srw/pl/issue220795/MainActivity.java
@@ -1,13 +1,47 @@
 package srw.pl.issue220795;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_drawer);
+
+        setupList();
     }
+
+    private void setupList() {
+        RecyclerView list = (RecyclerView) findViewById(R.id.history);
+        list.setLayoutManager(new LinearLayoutManager(this));
+
+        list.setAdapter(new RecyclerView.Adapter() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.list_item, parent, false);
+                return new RecyclerView.ViewHolder(view) {
+                };
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+                TextView text = (TextView) holder.itemView.findViewById(R.id.text);
+                text.setText("test 1");
+            }
+
+            @Override
+            public int getItemCount() {
+                return 2;
+            }
+        });
+    }
+
 }
